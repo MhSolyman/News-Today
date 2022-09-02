8122b60b5d8645ec16b14f47a849874b61faf457
MhSolyman <solymanbijoye.com>
2022-09-03 03:54:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const AllNewsCategory = () =>{
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+
+        .then(res => res.json())
+        .then(data => a(data.data.news_category));
+}
+AllNewsCategory()
+
+const a =(b)=>{
+    let count=0;  
+    let ul=document.getElementById("navbar-nav")
+for(const c of b)
+{
+    let li=document.createElement('li');
+    li.classList.add('nav-item')
+    li.innerHTML=`
+    <a onclick="newsCategory('${c.category_id}')" class="nav-link" href="#">${c.category_name}</a>
+  `
+ ul.appendChild(li)
+    
+    
+  count++;   
+   
+}
+let num=document.getElementById('num').innerHTML=`<h3></h3>`;
+}
+
+const newsCategory=(category_id)=>{
+    fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+
+        .then(res => res.json())
+        .then(data => console.log(data));
+
+}
+
+
+
+
+
