8c4e55956ca317f896174dc0614a11a635ff55a9
MhSolyman <solymanbijoye.com>
2022-09-03 19:39:53 +0600

    add item number

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ const AllNewsCategory = () =>{
 }
 AllNewsCategory()
 
-const AllNewsCategoryOne =(b)=>{
-    let count=0;  
+const AllNewsCategoryOne =(dataAll)=>{
+    
     let ul=document.getElementById("navbar-nav")
-for(const c of b)
+for(const data of dataAll)
 {
     let li=document.createElement('li');
     li.classList.add('nav-item')
     li.innerHTML=`
     
-    <a onclick="newsCategory('${c.category_id}')" class="nav-link" href="#">${c.category_name}</a>
+    <a onclick="newsCategory('${data.category_id}')" class="nav-link" href="#">${data.category_name}</a>
   `
  ul.appendChild(li)
     
     
-  count++;   
+    
    
 }
-let num=document.getElementById('num').innerHTML=`<h3></h3>`;
+
 }
 
 const newsCategory=(category_id)=>{
@@ -36,6 +36,11 @@ const newsCategory=(category_id)=>{
 
 }
 let news=(data)=>{
+    if(data==''){
+        console.log('no data')
+        document.getElementById('data-find').innerText='No data found'
+    }
+    count=0
     let card=document.getElementById("card");
     card.innerText='';
     for(const c of data){
@@ -64,9 +69,14 @@ let news=(data)=>{
 
   </div>
 </div>`;
- card.appendChild(dli)  
+ card.appendChild(dli);
+ let countOne =count++ ;
+ countOne= countOne+1; 
+ document.getElementById('data-find').innerText=`${count}  items found for category Entertainmen`
+;
+console.log(countOne)
     }
-
+   
 
 }
 
