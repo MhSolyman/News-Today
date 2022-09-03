afdaf709b19381d68f73a32d9e78f970893af8d8
MhSolyman <solymanbijoye.com>
2022-09-03 14:27:38 +0600

    add img

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ for(const c of b)
     let li=document.createElement('li');
     li.classList.add('nav-item')
     li.innerHTML=`
+    
     <a onclick="newsCategory('${c.category_id}')" class="nav-link" href="#">${c.category_name}</a>
   `
  ul.appendChild(li)
@@ -33,8 +34,25 @@ const newsCategory=(category_id)=>{
 
 }
 let news=(data)=>{
+    let card=document.getElementById("card");
+    card.innerText='';
     for(const c of data){
         console.log(c)
+        let dli=document.createElement('div');
+        dli.classList.add('col-12')
+    dli.innerHTML=`<div class="row border bg-light d-flex align-items-center "><img class="col-lg-6 col-md-6  col-sm-12 "
+    src="${c.image_url}" alt="">
+  <div class="col-lg-6 col-md-6  col-sm-12" >
+    <h1><b>'${c.title}'</b></h1>
+    <p>${c.details}</p>
+
+
+  </div>
+</div>`;
+ card.appendChild(dli)
+
+        
+       
     }
 
 
